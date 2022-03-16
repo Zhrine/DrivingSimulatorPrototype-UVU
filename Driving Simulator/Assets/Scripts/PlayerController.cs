45c47d6a2077f320a218654e363cc78eba628652
Foster Boone <10787343@uvu.edu>
2022-03-15 20:48:03 -0600

    Movement and turning Controllable!
    Went all the way through lesson 1.4

--- Driving Simulator/Assets/Scripts/PlayerController.cs
+++ Driving Simulator/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20.0f; 
+    private float turnspeed = 45.0f; 
+    private float horizontalInput; 
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up * turnspeed * horizontalInput * Time.deltaTime);
     }
 }
